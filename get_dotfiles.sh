ab0dc6ed3ce00611af106d15a3994db1846d58bc
JM Plourde <jmichel.plourde@gmail.com>
2021-03-14 15:40:17 -0400

    Replace copying for a symlink script
    Instead of having 2 copies on the computer, the files inside the repo are symlinked into the dirctory.

--- get_dotfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp ~/.zshrc ./zsh/
-cp -R ~/.config/i3 ./
-cp -R ~/.config/tmuxinator/ ./
-cp /usr/local/bin/monitor.sh ./i3/
-cp -R ~/.config/rofi/ ./
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# zsh
+ZSHDIR=~/.zshrc
+if ! [ -L $ZSHDIR ]; then
+  ln -s ${BASEDIR}/.zshrc $ZSHDIR
+else
+  echo $ZSHDIR "symlink already exists"
+fi
+
+# i3
+I3DIR=~/.config/i3
+if ! [ -L $I3DIR ]; then
+  ln -s ${BASEDIR}/i3/ $I3DIR
+else
+  echo $I3DIR "symlink already exists"
+fi
+
+# tmuxinator
+TMUXDIR=~/.config/tmuxinator
+if ! [ -L $TMUXDIR ]; then
+  ln -s ${BASEDIR}/tmuxinator/ $TMUXDIR
+else
+  echo $TMUXDIR "symlink already exists"
+fi
+
+# rofi
+ROFIDIR=~/.config/rofi
+if ! [ -L $ROFIDIR ]; then
+  ln -s ${BASEDIR}/rofi/ $ROFIDIR
+else
+  echo $ROFIDIR "symlink already exists"
+fi
