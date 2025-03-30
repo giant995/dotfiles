13a24f8f9adb3101865212bf46997c2f6300b9df
JPlourde <jean-michel.plourde@tlmgo.com>
2025-03-30 12:04:13 -0400

    Update .config/nvim/init.lua Update .zshrc

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -856,6 +856,31 @@ require("lazy").setup({
 		opts = { signs = false },
 	},
 
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = false,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").load_extension("lazygit")
+		end,
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
