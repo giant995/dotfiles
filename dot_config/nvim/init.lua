b6bf18a45dd94e7128b5c1cc69c450172e919070
JPlourde <jean-michel.plourde@tlmgo.com>
2025-04-16 11:46:12 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -982,6 +982,10 @@ require("lazy").setup({
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	-- { import = 'custom.plugins' },
+
+	-- Sets Neovim `commentstring` option based on the cursor location in the file. Fixes most empty `commentstring` and
+	-- languages with multiple styles for comments.
+	"JoosepAlviste/nvim-ts-context-commentstring",
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
